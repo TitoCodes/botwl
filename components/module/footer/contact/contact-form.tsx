82ee26192dc609a19e6b5793bef10c91361f89d9
TitoCodes <yourtitocodes@gmail.com>
2022-04-28 23:01:00 +0800

    Added submit functionality in contact-form

--- components/module/footer/contact/contact-form.tsx
+++ components/module/footer/contact/contact-form.tsx
@@ -1,27 +1,49 @@
+import { useState } from "react";
+
+type Contact = {
+    email: string,
+    subject: string,
+    body: string
+}
+
 const ContactForm = () => {
+    const [contact, setContact] = useState<Partial<Contact>>({});
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        setContact({});
+        event.target.reset();
+    }
+
+    const handleResetClick = async () => {
+        let form = (document.getElementById("contact-form") as HTMLFormElement);
+        form.reset();
+        setContact({});
+    }
+
     return (
         <>
-          <form>
-            <div className="flex text-amber items-center border-b text-amber-300 border-amber-100">
-              <input className="appearance-none bg-gray border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="email" aria-label="Email" />
-            </div>
-            <div className="flex items-center border-b text-amber-300 border-amber-100 py-2">
-              <input className="appearance-none bg-gray border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="subject" aria-label="Subject" />
-            </div>
-            <div className="flex items-center border-b text-amber-300 border-amber-100">
-              <textarea className="appearance-none bg-gray border-none w-full mr-3 py-5 px-2 focus:outline-none" placeholder="body" aria-label="Body" />
-            </div>
-            <div className='float-right pt-3'>
-              <button className="text-xs appearance-none hover:bg-green-400 text-white py-1 px-4 border border-green-400 hover:border-green-400 rounded">
-                Submit
-              </button>
-            </div>
-            <div className='float-right pt-3 pr-2'>
-              <button className="text-xs appearance-none hover:bg-red-400 text-white py-1 px-4 border border-red-400 hover:border-red-400 rounded">
-                Reset
-              </button>
-            </div>
-          </form>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div className="flex text-amber items-center border-b text-amber-300 border-amber-100">
+                    <input onChange={(e) => setContact({ ...contact, email: e.target.value })} required className="appearance-none bg-gray border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="email" name="email" aria-label="Email"/>
+                </div>
+                <div className="flex items-center border-b text-amber-300 border-amber-100 py-2">
+                    <input onChange={(e) => setContact({ ...contact, subject: e.target.value })} required className="appearance-none bg-gray border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="subject" name="subject" aria-label="Subject"/>
+                </div>
+                <div className="flex items-center border-b text-amber-300 border-amber-100">
+                    <textarea onChange={(e) => setContact({ ...contact, body: e.target.value })} required className="appearance-none bg-gray border-none w-full mr-3 py-5 px-2 focus:outline-none" placeholder="body" name="body" aria-label="Body" />
+                </div>
+                <div className='float-right pt-3'>
+                    <button className="text-xs appearance-none hover:bg-green-400 text-white py-1 px-4 border border-green-400 hover:border-green-400 rounded" type="submit">
+                        Submit
+                    </button>
+                </div>
+                <div className='float-right pt-3 pr-2'>
+                    <button onClick={handleResetClick} className="text-xs appearance-none hover:bg-red-400 text-white py-1 px-4 border border-red-400 hover:border-red-400 rounded" type="reset">
+                        Reset
+                    </button>
+                </div>
+            </form>
         </>
     )
 }
