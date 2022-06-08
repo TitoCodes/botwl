ea0c38b823d7dfa561aeb52ccf5c5776a27c211c
TitoCodes <yourtitocodes@gmail.com>
2022-06-08 11:41:00 +0800

    Updated overcooked playlist link

--- data/gameList.tsx
+++ data/gameList.tsx
@@ -15,7 +15,7 @@ export const gameList = [
     new GameModel("A Way Out", AWayOutImage, "A Way Out is an action-adventure game developed by Hazelight Studios and published by Electronic Arts.", GameStatus.NowPlaying, "https://youtube.com/playlist?list=PLLAhYom1S-4MEuTtyClqx-0BkbXlNpxuw"),
     new GameModel("Stardew Valley", StardewValleyImage, "Stardew Valley is a simulation role-playing video game developed by Eric 'ConcernedApe' Barone.", GameStatus.New),
     new GameModel("Don't Starve Together", DontStarveTogetherImage, "Don't Starve Together is the sequel to Don't Starve, and features the requested multiplayer mechanics.", GameStatus.New),
-    new GameModel("Overcooked 2", OverCookedTwoImage, "Overcooked 2 is a cooperative cooking simulation video game developed by Team17 alongside Ghost Town Games, and published by Team17. ", GameStatus.NowPlaying),
+    new GameModel("Overcooked 2", OverCookedTwoImage, "Overcooked 2 is a cooperative cooking simulation video game developed by Team17 alongside Ghost Town Games, and published by Team17. ", GameStatus.NowPlaying, "https://youtube.com/playlist?list=PLLAhYom1S-4N8ltCcYCBYplrYqd7xRJm-"),
     new GameModel("Minecraft Dungeons", MinecraftImage, "Minecraft Dungeons is a 2020 dungeon crawler video game developed by Mojang Studios and Double Eleven and published by Xbox Game Studios.", GameStatus.NowPlaying, "https://youtube.com/playlist?list=PLLAhYom1S-4OWIHqYRIU71ueComqnma8N"),
     new GameModel("Unravel Two", UnravelTwoImage, "Unravel Two is a puzzle-platform game developed by Swedish studio Coldwood Interactive and published by Electronic Arts.", GameStatus.New),
 ];
